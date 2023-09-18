f762afd1b1656caaebc22443b203aca38a2f2294
Jessica <jess91kr@gmail.com>
2023-09-18 09:56:51 +0100

    update bdd

--- MedAssistOnline/MedAssistOnline/settings.py
+++ MedAssistOnline/MedAssistOnline/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'MedAssistOnline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MAO',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+        
     }
 }
 
