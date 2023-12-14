2a30b06c1b7b48aea15433094c132ba8ea55b9c3
Jessica <jess91kr@gmail.com>
2023-12-14 15:56:09 +0100

    update settings

--- RStudy/RStudy/settings.py
+++ RStudy/RStudy/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home',
+    #'home',
     'RStudy.user',
     'RStudy.forum',
+    'home.apps.HomeConfig'
     #'RStudy.notes',
     'rest_framework',
     'rest_framework_simplejwt',
